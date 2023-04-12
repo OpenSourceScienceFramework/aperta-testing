ce75886c8e8a066a01a13bbe9c6ef0b979810e16
michael-voropanov <michael.voropanov@gmail.com>
2023-04-12 15:22:47 +0000

    feat: added a whole lot of things

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,16 @@
 import { type AppType } from "next/app";
-
+import { dark } from "@clerk/themes";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+const appearance = { baseTheme: dark };
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return (
-    <ClerkProvider {...pageProps}>
-      <Component {...pageProps} />
-    </ClerkProvider>
-  );
+    return (
+            <ClerkProvider appearance={appearance} {...pageProps}>
+            <Component {...pageProps} />
+            </ClerkProvider>  );
 };
 
 export default api.withTRPC(MyApp);
